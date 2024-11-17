e93cd151fc475d71ae3c4ec3bff9db5d720e59ef
Viktoriya Bokova <Viktoriya.Bokova@acronis.com>
2024-11-17 13:56:57 +0300

    new

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,22 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"net/http"
+)
+
+// Обработчик HTTP-запросов
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
 
 func main() {
-	// и здесь тоже
+	// Регистрируем обработчик для пути "/"
+	http.HandleFunc("/", handler)
+
+	// Запускаем веб-сервер на порту 8080
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,26 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	// и здесь тоже
+	http.HandleFunc("/api/user", userHandler)
+	fmt.Println("Server listening on port 9000")
+	http.ListenAndServe(":9000", nil)
 }
 
-// и тут тоже (если очень надо)
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	name := query.Get("name")
+
+	if name == "" {
+		fmt.Fprintln(w, "Please provide a name using the 'name' parameter in the URL.")
+		return
+	}
+
+	// Используйте переданное имя, а не "Salviya"
+	greeting := fmt.Sprintf("Hello, %s!", name)
+	fmt.Fprintln(w, greeting)
+}
